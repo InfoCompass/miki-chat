444078b1d915a8ec50c6ffc300c64ccf0617c381
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2021-01-06 13:13:36 +0100

    Basic proof of concept connecting to BfZ backend.

--- data/filter_questions/actions.py
+++ data/filter_questions/actions.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
 from typing import Any, Dict, List, Text, Optional
+import json
+
+from aiohttp import ClientSession
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -12,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 FILTER_MAPPING_PATH = 'data/filter_questions/entities/filter_mapping.csv'
 BFZ_URL = 'https://www.beratungsnetz-migration.de'
+BFZ_API_URL = 'https://api.beratungsnetz-migration.de'
 
 class ActionFilterResults(Action):
     """Display the results of a Filter Question request"""
@@ -37,7 +41,20 @@ class ActionFilterResults(Action):
             return f'{", ".join(init)} und {last}'
         return filters
 
-    def run(
+    # TODO:
+    #   - Handle exceptions in request
+    #   - Basic copy
+    #   - More fancy copy
+    async def _num_bfz_documents(self, filters):
+        async with ClientSession() as session:
+            url = f'{BFZ_API_URL}/actions/exportItems?format=JSON&keys=id&tags={",".join(filters)}'
+            resp = await session.request(method="GET", url=url)
+            resp.raise_for_status()
+            res = await resp.text()
+            num_documents = len(json.loads(res))
+        return num_documents
+
+    async def run(
             self,
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -64,7 +81,14 @@ class ActionFilterResults(Action):
             url_filters = ''.join(url_filters)
             url = f'{BFZ_URL}/list{url_filters}'
 
-            dispatcher.utter_message(text=f'Ich habe Angebote gefunden für: {display_filters}')
-            dispatcher.utter_message(text=f'Die Ergebnisse stehen hier zu verfügung {url}')
+            dispatcher.utter_message(text=f'Du hast gefragt nach Angebote für: {display_filters}')
+
+            num_documents = await self._num_bfz_documents(filters)
+
+            if num_documents:
+                dispatcher.utter_message(text=f'Es gibt {num_documents} ergebnisse zu Verfügung')
+                dispatcher.utter_message(text=f'Du kannst die hier erreichen {url}')
+            else:
+                dispatcher.utter_message(text=f'Es wurde leider keine Dokumenten gefunden')
 
         return []
